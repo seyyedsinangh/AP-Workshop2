4188b995044f1e8a8a4c135a7c321e6dd51ddefe
seyyed <sina.seyyed.2004@gmail.com>
2023-03-03 20:10:26 +0330

    Creating Student Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Student std1 = new Student("Ryan", "Gosling", "0987654");
+        Student std2 = new Student("Dennis", "Ritchie", "1234567");
+
+        std1.setGrade(18);
+        std2.setGrade(17.5);
+
+        std1.printStudentInfo();
+        std2.printStudentInfo();
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,61 @@
+public class Student {
+    private String firstname;
+    private String lastname;
+    //student's id contains at least 7 digits
+    private String id;
+    private double grade;
+
+    public Student(String firstname, String lastname, String id) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.id = id;
+        //the student's first grade is zero in the begining
+        grade = 0;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setId(String id) {
+        //the id can't be less than 7 digits
+        if (id.length()<7) {
+            System.out.println("The id is not valid");
+            return;
+        }
+        this.id = id;
+    }
+
+    public void setGrade(double grade) {
+        //the grade can't be less than 0 or more than 20
+        if (grade>20 || grade<0) {
+            System.out.println("The grade is not valid");
+            return;
+        }
+        this.grade = grade;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void printStudentInfo() {
+        System.out.println(firstname + " " + lastname + "\nID:" + id + "\nGRADE:" + grade);
+    }
+}
